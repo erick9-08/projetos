98fb4c050a3237bcb035ac4305adee6c01354a50
Erick sousa oliveira <167812734+erick9-08@users.noreply.github.com>
2024-11-03 13:00:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,16 @@ const paragraphCount = document.querySelector("[data-paragraph-count]");
 
 input.addEventListener("input", function () {
   if (input.value) {
-    // Count Words
     const wordsArray = input.value.split(" ").filter((word) => word !== "");
     wordCount.innerText = wordsArray.length;
 
     // Count Characters
     characterCount.innerText = input.value.length;
 
-    // Count Sentences
     const sentenceArray = input.value.split(/[.!]/);
 
     sentenceCount.innerText = sentenceArray.length - 1;
 
-    // Count Paragraph
     const paragraphArray = input.value
       .split("\n")
       .filter((p) => p.trim() !== "");
